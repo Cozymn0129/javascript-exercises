a01b0336cd25eb693118cab45193e677344fb899
Sho <cozymncfc0401@gmail.com>
2025-09-21 21:57:23 +0900

    javascript-exercises

--- 02_addNumbers/addNumbers.js
+++ 02_addNumbers/addNumbers.js
@@ -1,13 +1,10 @@
-function addNumbers() {
-  const a = 1;
-  const b = 1;
-
-  let result;
-
-  result = "a" + "b"; // <------ EDIT THIS LINE
-
-  return result;
+function numberChecker(number) {
+    if (number == 10 || number > 10) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-// Do not change this
-module.exports = addNumbers;
+
+module.exports = numberChecker;
